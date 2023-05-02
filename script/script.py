6af0f1eb43e0b5566c5816d6add5d7636d33c0b3
joao-fmr <107021546+joao-fmr@users.noreply.github.com>
2023-05-02 13:42:01 +0200

    docker, script et urls.txt

--- script/script.py
+++ script/script.py
@@ -2,7 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from datetime import datetime
 import time
+import pytz
+import requests
+import os
 
 options = webdriver.ChromeOptions()
 options.add_argument('--no-sandbox')
@@ -10,38 +14,68 @@ options.add_argument('--headless')
 options.add_argument('--disable-dev-shm-usage')
 
 driver = webdriver.Chrome(options=options)
-wind_site = "https://www.meteosuisse.admin.ch/static/measured-values-app/index.html#lang=fr&param=messwerte-windgeschwindigkeit-kmh-10min&station=PRE&chart=hour"
-temp_site = "https://www.meteosuisse.admin.ch/static/measured-values-app/index.html#lang=fr&param=messwerte-lufttemperatur-10min&chart=hour&station=PUY"
-precip_site = "https://www.meteosuisse.admin.ch/static/measured-values-app/index.html#lang=fr&param=messwerte-niederschlag-10min&chart=hour&station=LSN"
 
-driver.get(wind_site)
+timezone = pytz.timezone('Europe/Paris')
+now = datetime.now(timezone)
+date = now.strftime("%Y-%m-%d %H:%M:%S")
+
+api_url = os.environ['API_URL']
+wind_url = os.environ['WIND_URL']
+temp_url = os.environ['TEMP_URL']
+precip_url = os.environ['PRECIP_URL']
+div_class = os.environ['DIV_CLASS']
+value_class = os.environ['VALUE_CLASS']
+
+driver.get(wind_url)
 driver.execute_script("location.reload()")
 
-def extract_value(driver):
+
+def extract_value(driver, label, unit):
     time.sleep(1)
-    div = driver.find_element(By.XPATH, '//div[@class="measurement-map__detail-body"]')
-    value = div.find_element(By.XPATH, './/div[@class="measurement-map__detail--value"]')
-    return value.text
+
+    div = driver.find_element(By.XPATH, div_class)
+    value = div.find_element(By.XPATH, value_class)
+    value = float(value.text.split()[0]) 
+
+    print(f"{label}: {value}{unit} {date}")
+
+    return value
 
 
-wind = extract_value(driver)
-print("Vitesse du vent: " + wind)
+wind = extract_value(driver, "Vitesse du vent", "km/h")
 
 button = driver.find_element(By.CSS_SELECTOR, 'label[for="pill_subParams1"]')
 button.click()
 
+gust = extract_value(driver, "Raffale", "km/h")
 
-gust = extract_value(driver)
-print("Rafale: " + gust)
 
-driver.get(temp_site)
+driver.get(temp_url)
 driver.execute_script("location.reload()")
-temp = extract_value(driver)
-print("Température: " + temp)
 
-driver.get(precip_site)
+temp = extract_value(driver, "Température", "°")
+
+
+driver.get(precip_url)
 driver.execute_script("location.reload()")
-precip = extract_value(driver)
-print("Précipitation: " + precip)
+
+precip = extract_value(driver, "Précipitation", "mm")
 
 driver.close()
+
+
+data = {
+    'wind': wind,
+    'gust': gust,
+    'temperature': temp,
+    'precipitation': precip,
+    'date' : date
+}
+
+response = requests.post(api_url, data=data)
+
+if response.status_code == 201:
+    print('Données insérées avec succès')
+else:
+    print('Erreur lors de l\'insertion des données')
+    print(response.text)
