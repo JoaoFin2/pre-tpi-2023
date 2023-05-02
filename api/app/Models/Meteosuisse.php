2de5bdca61979ea39a94f4c528bfa1f7c6a26612
joao-fmr <107021546+joao-fmr@users.noreply.github.com>
2023-05-02 09:17:08 +0200

    meteosuisse

--- api/app/Models/Meteosuisse.php
+++ api/app/Models/Meteosuisse.php
@@ -2,11 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class MeteoSuisse extends Model
 {
+    use HasFactory;
+
+    public $timestamps = false;
+
     protected $fillable = [
         'date', 'temperature', 'precipitation', 'date'
     ];
+
 }
--- api/database/migrations/2023_05_02_071556_create_meteo_suisses_table.php
+++ api/database/migrations/2023_05_02_071556_create_meteo_suisses_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('meteo_suisse', function (Blueprint $table) {
+        Schema::create('meteo_suisses', function (Blueprint $table) {
             $table->id();
             $table->float('wind');
             $table->float('gust');
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('meteo_suisse');
+        Schema::dropIfExists('meteo_suisses');
     }
 };
