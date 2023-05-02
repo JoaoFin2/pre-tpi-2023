39cd9a40e31a8539fc8792d91eedee69f0abd8fa
joao-fmr <107021546+joao-fmr@users.noreply.github.com>
2023-05-02 09:12:30 +0200

    meteosuisse

--- api/app/Models/Meteosuisse.php
+++ api/app/Models/Meteosuisse.php
@@ -6,8 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class MeteoSuisse extends Model
 {
-    public $timestamps = false;
-
     protected $fillable = [
         'date', 'temperature', 'precipitation', 'date'
     ];
